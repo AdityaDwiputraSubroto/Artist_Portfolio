8e10da58193b904975cbf60aebda5bae8aa2c28c
AdityaDwiputraSubroto <adityadwiputras23@gmail.com>
2023-05-13 09:00:24 +0700

    feat: add HomePage

--- pages/public/index.php
+++ pages/public/index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="../../styles/icons.css" />
     <link rel="stylesheet" href="../../styles/navbar.css" />
     <link rel="stylesheet" href="../../styles/footer.css" />
+    <link rel="stylesheet" href="../../styles/HomePage.css">
     <title>Home</title>
     <style>
         * {
@@ -32,10 +33,50 @@
     <?php
     include '../../components/contact-icon.php';
     ?>
+    <div class="mt-4 position-relative mb-4">
+        <div class="scroll-images">
+            <div class="image"> <img src="../../assets/img/Gambar2.png" class="" alt="..."></div>
+            <div class="image"> <img src="../../assets/img/Gambar1.png" class="" alt="..."></div>
+            <div class="image"> <img src="../../assets/img/Gambar3.png" class="" alt="..."></div>
+            <div class="image"> <img src="../../assets/img/Gambar1.png" class="" alt="..."></div>
+            <div class="image"> <img src="../../assets/img/Gambar3.png" class="" alt="..."></div>
+            <div class="image"> <img src="../../assets/img/Gambar2.png" class="" alt="..."></div>
+            <div class="image"> <img src="../../assets/img/Gambar1.png" class="" alt="..."></div>
+            <div class="image"> <img src="../../assets/img/Gambar3.png" class="" alt="..."></div>
+        </div>
+        <button class="arrow left-arrow"><img class="rotate-180" src="../../assets/img/icons/right-arrow.png" alt="left-arrow">;</button>
+        <button class="arrow right-arrow"><img src="../../assets/img/icons/right-arrow.png" alt="right-arrow">;</button>
+    </div>
 
     <?php
     include '../../components/footer.php';
     ?>
 </body>
+<script>
+    const scrollImages = document.querySelector('.scroll-images');
+    const leftArrow = document.querySelector('.left-arrow');
+    const rightArrow = document.querySelector('.right-arrow');
+
+    leftArrow.addEventListener('click', () => {
+        scrollImages.scrollLeft -= 300;
+    });
+
+    rightArrow.addEventListener('click', () => {
+        scrollImages.scrollLeft += 300;
+    });
+
+    scrollImages.addEventListener('scroll', () => {
+        if (scrollImages.scrollLeft === 0) {
+            leftArrow.classList.add('disabled');
+        } else {
+            leftArrow.classList.remove('disabled');
+        }
+        if (scrollImages.scrollLeft + scrollImages.clientWidth === scrollImages.scrollWidth) {
+            rightArrow.classList.add('disabled');
+        } else {
+            rightArrow.classList.remove('disabled');
+        }
+    });
+</script>
 
 </html>
