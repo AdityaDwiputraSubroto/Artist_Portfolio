c4cd309c639bbe057a07fc021a3dbb3cfa2a9641
AdityaDwiputraSubroto <adityadwiputras23@gmail.com>
2023-05-24 17:32:39 +0700

    add navbar logout button

--- components/adminComponents/navbar.php
+++ components/adminComponents/navbar.php
@@ -10,13 +10,18 @@
             "LOGOUT" => "LogoutProcess.php"
         );
 
-        foreach ($navbarItems as $navName => $navLink) { ?>
-            <li><a class=" 
+        foreach ($navbarItems as $navName => $navLink) {
+            if ($navName != 'LOGOUT') { ?>
+                <li><a class="
         <?php
-            if ($activePage == $navName) {
-                echo "active";
-            }
+                if ($activePage == $navName) {
+                    echo "active";
+                }
         ?>" href="<?php echo $navLink ?>"><?php echo $navName ?></a></li>
+            <?php } else { ?>
+                <li><a href="<?php echo $navLink ?>"><button class="btn btn-primary"><?php echo $navName ?></button></a></li>
+            <?php } ?>
         <?php } ?>
+
     </ul>
 </nav>
